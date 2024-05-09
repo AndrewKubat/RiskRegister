850495a68b5069d04a07561f5264a04ee4bbf5b2
Andrew Kubat <94582866+akubat@users.noreply.github.com>
2024-05-09 02:06:09 -0400

    Initial file creation

---
+++ RiskRegisterCSV.py
@@ -0,0 +1,5 @@
+##Author: Andrew Kubat
+##Purpose: CSV portion of the Risk Register Automated Management System (RRAMS). This file may be unused, depending on how development goes.
+
+def main():
+    print("Might be deleted soon, still planning the project out.")
---
+++ RiskRegisterMain.py
@@ -0,0 +1,16 @@
+##Author: Andrew Kubat
+##Purpose: Main portion of the Risk Register Automated Management System (RRAMS). This file will run all necessary code
+
+
+##This module runs and determines what other modules will be run.
+def main():
+    print("In Progress, check back later.")
+
+
+##This module is used to generate the initial GUI
+
+##This module is used to determine GUI navigation
+
+##This module will be used to enter CSV data
+
+##This module will be used to enter SQL data
